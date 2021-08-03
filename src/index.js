cdab1fab682dc7f0c8cda490a31c08cd9611d25d
unknown <Roman Savchuk@dell-kip19.bmk.by>
2021-08-04 00:13:46 +0300

    refactor: added code js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(array === 0){
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === 0){
+    return 0;
+  } else {
+    return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === 0){
+    return 0;
+  } else {
+    return array.reduce((acc, amount) => acc + amount) / array.length;
+  }
 }
