71b766c9a62cbde22f2496e17961a2d00f49b81d
vadimlasevich <70883110+vadimlasevich@users.noreply.github.com>
2021-08-05 19:12:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-exports.min = function min (array) {
-  if(array === 0){
-    return 0;
-  } else {
-    return Math.min(...array);
-  }
+exports.min = function min(array) {
+	if (array == 0 || array == undefined || array.length == 0) {
+		return 0;
+	} else {
+		return Math.min(...array);
+	}
 }
 
-exports.max = function max (array) {
-  if(array === 0){
-    return 0;
-  } else {
-    return Math.max(...array);
-  }
+exports.max = function max(array) {
+	if (array == 0 || array == undefined || array.length == 0) {
+		return 0;
+	} else {
+		return Math.max(...array);
+	}
 }
 
-exports.avg = function avg (array) {
-  if(array === 0){
-    return 0;
-  } else {
-    return array.reduce((acc, amount) => acc + amount) / array.length;
-  }
+exports.avg = function avg(array) {
+	if (array == 0 || array == undefined || array.length == 0) {
+		return 0;
+	} else {
+		return array.reduce((acc, amount) => acc + amount) / array.length;
+	}
 }
